6d4c795f30455a7bfb50761762d5e6dab8bd7eb2
Mark Alexander <dracunos72@hotmail.com>
2015-07-02 18:06:22 -0700

    abstracted filepaths

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,22 @@
+import os
 from flask import Flask, render_template, redirect, request
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, RadioField, validators
 from random import randint
 from mods import journeygame
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+
 @app.route('/')
 def index():
-    x = open('mysite/textdata/post.txt', 'r')
+    x = open(os.path.join(basedir, 'textdata/post.txt'), 'r')
     filer = x.readlines()
     x.close()
     return render_template('index.html', filer=filer)
+
 
 @app.route('/form', methods=['GET', 'POST'])
 def myform():
@@ -20,11 +24,12 @@ def myform():
     if form.validate_on_submit():
         poster = form.poster.data
         posted = form.posted.data
-        x = open('mysite/textdata/post.txt', 'a')
+        x = open(os.path.join(basedir, 'textdata/post.txt'), 'a')
         x.write('\n' + poster + ': ' + posted)
         x.close()
         return redirect('/')
     return render_template('myform.html', form=form, ttl='New Post')
+
 
 @app.route('/test')
 def test():
@@ -33,29 +38,32 @@ def test():
     <br> Link from var: {}
     """.format(tt)
 
+
 @app.route('/journey')
 def journey():
-    page = request.args.get('pg','1')
+    page = request.args.get('pg', '1')
     textlist = journeygame.pages[int(page)]['text']
     links = journeygame.pages[int(page)]['links']
     if page == '1':
-        stats = [randint(3,18),randint(3,18),randint(3,18)]
+        stats = [randint(3, 18), randint(3, 18), randint(3, 18)]
         return render_template('journey.html', ttl="Journey of Distangibility",
-                           story=textlist, links=links, stats=stats)
+                               story=textlist, links=links, stats=stats)
     return render_template('journey.html', ttl="Journey of Distangibility",
                            story=textlist, links=links)
+
 
 @app.route('/hangman', methods=['GET', 'POST'])
 def hangedman():
     form = HMForm()
     formnew = NHMForm()
 
-    x = open('mysite/textdata/hmdata.txt', 'r')
+    x = open(os.path.join(basedir, 'textdata/hmdata.txt'), 'r')
     hmdata = x.readlines()
     x.close()
-    x = open('mysite/textdata/hmwords.txt', 'r')
+    x = open(os.path.join(basedir, 'textdata/hmwords.txt'), 'r')
     wordlist = x.readlines()
     x.close()
+    category = None
     for i in wordlist:
         for q in i.split():
             if hmdata[0][0:-1] == q:
@@ -69,9 +77,9 @@ def hangedman():
     won = True
     for i in hmdata[0][0:-1]:
         if i in hmdata[1]:
-            word = word + i
+            word += i
         else:
-            word = word + ' _ '
+            word += ' _ '
             won = False
 
     missed = 0
@@ -83,14 +91,14 @@ def hangedman():
         lost = True
 
     if formnew.validate_on_submit() and request.form['btn'] == 'New Game':
-        hmwords = open('mysite/textdata/hmwords.txt', 'r')
+        hmwords = open(os.path.join(basedir, 'textdata/hmwords.txt'), 'r')
         wordlist = hmwords.readlines()
         hmwords.close()
         for i in wordlist:
             l = i.split()
             if formnew.gametype.data == l[0]:
-                newgame = l[randint(1,len(l)-1)]
-                x = open('mysite/textdata/hmdata.txt', 'w+')
+                newgame = l[randint(1, len(l)-1)]
+                x = open(os.path.join(basedir, 'textdata/hmdata.txt'), 'w+')
                 x.write(newgame + '\n')
                 x.close()
                 return redirect('/hangman')
@@ -110,9 +118,11 @@ def hangedman():
                                    category=category, ttl='Hangman')
         if hminput in hmdata[1]:
             return render_template('hangedman.html', form=form, word=word,
-                                   tried=tried, missed=missed, alreadypick=True,
-                                   category=category, ttl='Hangman')
-        x = open('mysite/textdata/hmdata.txt', 'a')
+                                   tried=tried, missed=missed,
+                                   alreadypick=True, category=category,
+                                   ttl='Hangman')
+        x = open(os.path.join(basedir, 'textdata/hmdata.txt'), 'a')
+        hminput = str(hminput)
         x.write(hminput)
         x.close()
         return redirect('/hangman')
@@ -125,12 +135,19 @@ class MyForm(Form):
     poster = StringField('poster')
     posted = StringField('posted')
 
+
 class HMForm(Form):
     hminput = StringField('hminput', [validators.Length(min=1, max=1)])
 
+
 class NHMForm(Form):
     gametype = RadioField(
-        'gametype', choices=[('animals','Animals (easy)'),('food','Food (easy)'),
-        ('technology','Technology'),('countries','Countries')],
-        default = 'animals'
-    )
+        'gametype', choices=[('animals', 'Animals (easy)'),
+                             ('food', 'Food (easy)'),
+                             ('technology', 'Technology'),
+                             ('countries', 'Countries')],
+        default='animals')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
